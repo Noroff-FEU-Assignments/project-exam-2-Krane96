d2f651b44d98aec2542267165413158b713da820
Krane96 <69842760+Krane96@users.noreply.github.com>
2022-04-01 18:54:37 +0200

    test

---
+++ src/components/navbar/NavigationBar.jsx
@@ -0,0 +1,43 @@
+import {
+  Navbar,
+  Nav,
+  Form,
+  Button,
+  Container,
+  NavDropdown,
+  FormControl,
+} from "react-bootstrap";
+const NavigationBar = () => {
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link>Home</Nav.Link>
+              <Nav.Link>Login</Nav.Link>
+              <Nav.Link>Link</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavigationBar;
