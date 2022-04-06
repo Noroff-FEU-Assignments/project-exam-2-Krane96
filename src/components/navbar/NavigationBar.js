9140540900e88efc65e964e8b96401833d6f17e2
Krane96 <69842760+Krane96@users.noreply.github.com>
2022-04-06 16:15:30 +0200

    ok

--- src/components/navbar/NavigationBar.js
+++ src/components/navbar/NavigationBar.js
@@ -12,7 +12,7 @@ const NavigationBar = () => {
     <>
       <Navbar bg="light" expand="lg">
         <Container fluid>
-          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Brand href="#">Holidaze</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
