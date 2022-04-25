528d78fe39a529493ea00a4a72367837f3108319
Krane96 <69842760+Krane96@users.noreply.github.com>
2022-04-25 21:05:31 +0200

    carousel

--- src/components/carousels/CarouselHotels.js
+++ src/components/carousels/CarouselHotels.js
@@ -4,37 +4,38 @@ const CarouselHotels = () => {
   return (
     <>
     <Carousel>
-  <Carousel.Item interval={1000}>
+  <Carousel.Item interval={3000}>
     <img
+    
       className="d-block w-100"
-      src="holder.js/800x400?text=First slide&bg=373940"
+      src="/images/slider_1.jpg"
       alt="First slide"
     />
     <Carousel.Caption>
       <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      
     </Carousel.Caption>
   </Carousel.Item>
-  <Carousel.Item interval={500}>
+  <Carousel.Item interval={3000}>
     <img
       className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
+      src="/images/slider_2.jpg"
       alt="Second slide"
     />
     <Carousel.Caption>
       <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
+      src="/images/slider_3.jpg"
       alt="Third slide"
     />
     <Carousel.Caption>
       <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
