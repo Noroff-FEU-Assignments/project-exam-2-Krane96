a0df68d404e9fa92edcfe53eb02054f1c6c156d2
Krane96 <69842760+Krane96@users.noreply.github.com>
2022-05-28 22:37:22 +0200

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/home/Home";
 import Login from "./components/pages/login/Login";
 import Admin from "./components/pages/admin/Admin";
-
 import Details from "./components/pages/hotels/Details";
 import Contact from "./components/pages/contact/Contact";
 import Messages from "./components/pages/admin/Messages";
@@ -10,6 +9,7 @@ import Bookings from "./components/pages/admin/Bookings";
 import CreateBooking from "./components/pages/admin/CreateBooking";
 import NavigationBar from "./components/navbar/NavigationBar";
 import Footer from "./components/footer/Footer";
+import Hotels from "./components/pages/hotels/Hotels";
 import { AuthProvider } from "./utils/context";
 import HowItWorks from "./components/howitworks/HowItWorks";
 import "./styles/main.scss";
@@ -25,6 +25,7 @@ const App = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/admin" element={<Admin />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/hotels" element={<Hotels />} />
             <Route path="details/:id" element={<Details />} />
             <Route path="admin/messages" element={<Messages />} />
             <Route path="admin/bookings" element={<Bookings />} />
